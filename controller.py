18e8eee2951717406332c19dc3f0339d7db4f8f9
Leandro Maia <leandroc@inatel.br>
2021-10-19 19:07:06 -0300

    Refactor in the controller method responsible for show all data

--- controller.py
+++ controller.py
@@ -8,14 +8,16 @@ class PersonController:
         self.view =  PersonView()
 
     def show_all(self):
-        people_in_db = Person.get_all()
-        return self.view.show_all(people_in_db)
-
-    def start(self):
-        self.view.start_view()
-        response = self.view.ask_operation()
+        response = self.view.ask_read_operation()
 
         if response == 'y':
             return self.show_all()
         else:
             return self.view.end_view()
+
+        people_in_db = Person.get_all()
+        self.view.show_all(people_in_db)
+
+    def start(self):
+        self.view.start_view()
+        
--- view.py
+++ view.py
@@ -3,17 +3,17 @@ from model import Person
 
 class PersonView:
 
+    def start_view(self):
+        print('MVC - the simplest example')
+
+    def ask_read_operation(self):
+        return input('Do you want to see everyone in my db?[y/n]\n')
+
+    def end_view(self):
+        print('Goodbye!')
+
     def show_all(self, people):
         print(f'\nIn our db we have {len(people)} people. Here they are:')
 
         for item in people:
             print(item.full_name)
-
-    def start_view(self):
-        print('MVC - the simplest example')
-
-    def ask_operation(self):
-        return input('Do you want to see everyone in my db?[y/n]\n')
-
-    def end_view(self):
-        print('Goodbye!')
